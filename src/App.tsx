1ff4f2e74d05f52755471acc34a858b3a8a50c47
razzanYoni <13521087@mahasiswa.itb.ac.id>
2023-04-18 20:21:12 +0700

    testing

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ export const App = () => (
         <VStack spacing={8}>
           <Logo h="40vmin" pointerEvents="none" />
           <Text>
-            Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
+            Test
           </Text>
           <Link
             color="teal.500"
@@ -36,3 +36,5 @@ export const App = () => (
     </Box>
   </ChakraProvider>
 )
+
+export default App;
