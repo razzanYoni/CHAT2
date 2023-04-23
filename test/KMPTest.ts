7896b54d7fc5a3ce3b6f6ec119c99cbc120d413d
Fakhrimm <16521155@mahasiswa.itb.ac.id>
2023-04-23 20:33:00 +0700

    feat: add Knuth-Morris-Pratt algorithm
    add the KMP algorithm and the test unit for KMP

---
+++ test/KMPTest.ts
@@ -0,0 +1,14 @@
+import { KMP } from '../src/KMP';
+
+describe('KMP', () => {
+    it('should return the correct index of the first occurrence of pattern in text', () => {
+        expect(KMP('abcxabcdabcdabcy', 'abcdabcy')).toBe(8);
+        expect(KMP('abcxabcdabcdabcy', 'abcdabca')).toBe(-1);
+        expect(KMP('aaaaa', 'aa')).toBe(0);
+        expect(KMP('aaaaa', 'aaa')).toBe(0);
+        expect(KMP('aaaaa', 'aaaa')).toBe(0);
+        expect(KMP('aaaaa', 'aaaaa')).toBe(0);
+        expect(KMP('abababab', 'ababababab')).toBe(-1);
+        expect(KMP('abababab', '')).toBe(0);
+    });
+});
