5ba3a1051f51feff9944ad76eccf8e020d61d8f3
Fakhrimm <16521155@mahasiswa.itb.ac.id>
2023-05-02 09:52:33 +0700

    refactor: adjust whitespace diff

--- src/processing/KMP.ts
+++ src/processing/KMP.ts
@@ -5,33 +5,33 @@
  * @returns border function dari pattern
  */
 function computeBorder(pattern: string) {
-    const border = new Array(pattern.length).fill(0);
-    let i = 1;
-    let j = 0;
-    while (i < pattern.length) {
-        /* Kondisi awal pada tahap ini adalah P[0..j-1] sama dengan P[i-j..i-1] jika j > 0. */
-        if (pattern[i] === pattern[j]) {
-            border[i] = j + 1;
-            i++;
-            j++;
-        } else if (j > 0) {
-            j = border[j - 1];
-            /* Hal ini dilakukan karena pada tahap ini, kita sudah 
-            mengetahui bahwa karakter pada indeks j-1 sama dengan 
-            karakter pada indeks i-1, j-2 sama dengan i-2, dst.
-            
-            Karena kita sudah dapatkan prefix yang juga suffix 
-            terpanjang pada border[j-1], maka gunakan border[j-1] 
-            sebagai nilai awal untuk j.
+  const border = new Array(pattern.length).fill(0);
+  let i = 1;
+  let j = 0;
+  while (i < pattern.length) {
+    /* Kondisi awal pada tahap ini adalah P[0..j-1] sama dengan P[i-j..i-1] jika j > 0. */
+    if (pattern[i] === pattern[j]) {
+      border[i] = j + 1;
+      i++;
+      j++;
+    } else if (j > 0) {
+      j = border[j - 1];
+      /* Hal ini dilakukan karena pada tahap ini, kita sudah 
+      mengetahui bahwa karakter pada indeks j-1 sama dengan 
+      karakter pada indeks i-1, j-2 sama dengan i-2, dst.
+      
+      Karena kita sudah dapatkan prefix yang juga suffix 
+      terpanjang pada border[j-1], maka gunakan border[j-1] 
+      sebagai nilai awal untuk j.
 
-            Perhatikan untuk nilai baru j, tetap terjaga kondisi
-            bahwa karakter pada indeks j-1 sama dengan karakter 
-            pada indeks i-1, j-2 sama dengan i-2, dst.*/
-        } else {
-            i++;
-        }
+      Perhatikan untuk nilai baru j, tetap terjaga kondisi
+      bahwa karakter pada indeks j-1 sama dengan karakter 
+      pada indeks i-1, j-2 sama dengan i-2, dst.*/
+    } else {
+      i++;
     }
-    return border;
+  }
+  return border;
 }
 /**
  * 
@@ -40,21 +40,21 @@ function computeBorder(pattern: string) {
  * @returns mengembalikan indeks pertama dari pattern di text, -1 jika tidak ditemukan
  */
 export function KMP(text: string, pattern: string) {
-    const border = computeBorder(pattern);
-    let i = 0;
-    let j = 0;
-    while (i < text.length) {
-        if (text[i] === pattern[j]) {
-            if (j === pattern.length - 1) {
-                return i - j;
-            }
-            i++;
-            j++;
-        } else if (j > 0) {
-            j = border[j - 1];
-        } else {
-            i++;
-        }
+  const border = computeBorder(pattern);
+  let i = 0;
+  let j = 0;
+  while (i < text.length) {
+    if (text[i] === pattern[j]) {
+      if (j === pattern.length - 1) {
+        return i - j;
+      }
+      i++;
+      j++;
+    } else if (j > 0) {
+      j = border[j - 1];
+    } else {
+      i++;
     }
-    return -1;
+  }
+  return -1;
 }
--- src/processing/levenshtein.ts
+++ src/processing/levenshtein.ts
@@ -1,43 +1,43 @@
 function levenshetain_distance(s: string, t: string): number {
 
-    // Initialize matrix of zeros of size (s.length + 1) x (t.length + 1)
-    // distMatrix[i][j] represents the Levenshtein distance between the first i characters of s and the first j characters of t
-    let distMatrix = new Array(s.length + 1).fill(0).map(() => new Array(t.length + 1).fill(0));
+  // Initialize matrix of zeros of size (s.length + 1) x (t.length + 1)
+  // distMatrix[i][j] represents the Levenshtein distance between the first i characters of s and the first j characters of t
+  let distMatrix = new Array(s.length + 1).fill(0).map(() => new Array(t.length + 1).fill(0));
 
 
-    // Fill in the first row of the matrix
-    // The distance between the empty string and the first j characters of t is j
+  // Fill in the first row of the matrix
+  // The distance between the empty string and the first j characters of t is j
+  for (let j = 1; j <= t.length; j++) {
+    distMatrix[0][j] = j;
+  }
+
+  // Fill in the first column of the matrix
+  // The distance between the first i characters of s and the empty string is i
+  for (let i = 1; i <= s.length; i++) {
+    distMatrix[i][0] = i;
+  }
+
+  // Fill in the rest of the matrix
+  for (let i = 1; i <= s.length; i++) {
     for (let j = 1; j <= t.length; j++) {
-        distMatrix[0][j] = j;
+
+      // If the last characters of the substrings match, the cost is 0
+      let cost = 0;
+      if (s[i - 1] !== t[j - 1]) {
+        cost = 1;
+      }
+
+      distMatrix[i][j] = Math.min(
+        distMatrix[i - 1][j] + 1, // deletion or insertion
+        distMatrix[i][j - 1] + 1, // deletion or insertion
+        distMatrix[i - 1][j - 1] + cost // substitution
+      );
     }
+  }
 
-    // Fill in the first column of the matrix
-    // The distance between the first i characters of s and the empty string is i
-    for (let i = 1; i <= s.length; i++) {
-        distMatrix[i][0] = i;
-    }
-
-    // Fill in the rest of the matrix
-    for (let i = 1; i <= s.length; i++) {
-        for (let j = 1; j <= t.length; j++) {
-
-            // If the last characters of the substrings match, the cost is 0
-            let cost = 0;
-            if (s[i - 1] !== t[j - 1]) {
-                cost = 1;
-            }
-
-            distMatrix[i][j] = Math.min(
-                distMatrix[i - 1][j] + 1, // deletion or insertion
-                distMatrix[i][j - 1] + 1, // deletion or insertion
-                distMatrix[i - 1][j - 1] + cost // substitution
-            );
-        }
-    }
-
-    return distMatrix[s.length][t.length];
+  return distMatrix[s.length][t.length];
 }
 
 export function percentage_similarity(s: string, t: string): number {
-    return 1 - (levenshetain_distance(s, t) / Math.max(s.length, t.length));
+  return 1 - (levenshetain_distance(s, t) / Math.max(s.length, t.length));
 }
