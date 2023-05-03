fec2e62b537e0a7050b18bc5c9215deef7ca1bbd
Fakhrimm <16521155@mahasiswa.itb.ac.id>
2023-05-03 14:16:42 +0700

    feat: init main question handler
    There is still edge case not handled that is add question
    
    getAllQuestion still wrong

---
+++ processing/mainHandler.ts
@@ -0,0 +1,72 @@
+import { KMP } from './KMP'
+import { BM } from './BM'
+import { mathQuestionHandler } from './mathHandler'
+import { dateQuestionHandler } from './dateHandler'
+import { Dict } from '@chakra-ui/utils'
+import { similarityScore } from './levenshtein'
+
+export function mainQuestionHandler(pattern: string, isKMP: boolean): string {
+
+  // Handle date question
+  let [is_match, answer] = dateQuestionHandler(pattern)
+
+  if (is_match) {
+    return answer
+  } else if (!is_match && answer !== '0') {
+    return "Date is invalid: " + answer
+  }
+
+  // Handle math question
+  [is_match, answer] = mathQuestionHandler(pattern)
+
+  if (is_match) {
+    return "The answer is " + answer
+  }
+
+  let matchAlg = BM
+  if (isKMP) {
+    matchAlg = KMP
+  }
+
+  let questionAnwerDict = getAllQuestionAnswer()
+  for (let i = 0; i < questionAnwerDict.length; i++) {
+    let question = Object.keys(questionAnwerDict[i])[0]
+    if (question.length != pattern.length) {
+      continue
+    }
+    if (matchAlg(question, pattern) != -1) {
+      return Object.values(questionAnwerDict[i])[0]
+    }
+  }
+
+  let similarity: [number, number][] = []
+  for (let i = 0; i < questionAnwerDict.length; i++) {
+    let question = Object.keys(questionAnwerDict[i])[0]
+    similarity.push([similarityScore(question, pattern), i])
+  }
+
+  similarity.sort((a, b) => b[0] - a[0])
+
+  if (similarity[0][0] >= 0.9) {
+    return Object.values(questionAnwerDict[similarity[0][1]])[0]
+  } else {
+    let answerText = "Question not found in database.\n"
+    answerText += "Did you mean:\n"
+    for (let i = 0; i < 3 && i < similarity.length; i++) {
+      answerText += (similarity[i][0] * 100).toFixed(2) + " percent similar: "
+      answerText += Object.keys(questionAnwerDict[similarity[i][1]])[0] + "\n"
+    }
+    return answerText
+  }
+
+}
+
+
+function getAllQuestionAnswer(): Dict<string>[] {
+  // Code to get all question answer from database
+
+  return [{ 'What is the answer to life, the universe and everything?': '42' }]
+}
+
+let res = mainQuestionHandler('1+2', true)
+console.log(res)
