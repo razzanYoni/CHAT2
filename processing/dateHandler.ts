795fb2e4cb61211a2877cb78abaabf0a992dd5ee
Fakhrimm <16521155@mahasiswa.itb.ac.id>
2023-05-03 14:14:15 +0700

    fix: debugged dateHandler

--- processing/dateHandler.ts
+++ processing/dateHandler.ts
@@ -1,34 +1,41 @@
-export function dateQuestion(text: string): [boolean, string] {
-  let dateQuestion: RegExp = /\s*(kapan|hari\s+apa)?\s*(\d{1,2})\/(\d{1,2})\/(\d{1,5})[\s\?]*/gi;
+export function dateQuestionHandler(text: string): [boolean, string] {
+  let dateQuestion: RegExp = /^(\s*(kapan|hari\s+apa)?\s*(\d{1,2})\/(\d{1,2})\/(\d{1,5})[\s\?]*)$/gi;
 
   let isDateQuestion = dateQuestion.test(text);
+  dateQuestion.lastIndex = 0;
   let date = dateQuestion.exec(text);
 
-  if (isDateQuestion && date) {
-    let dateParsed = [parseInt(date[2]), parseInt(date[3]), parseInt(date[4])];
-    if (validateDate(dateParsed)) {
-      return [true, gaussAlgorithm(dateParsed)];
-    } else {
-      return [false, '1'];
-    }
-  } else {
+  if (!(isDateQuestion && date)) {
     return [false, '0'];
   }
+
+  let dateParsed = [parseInt(date[3]), parseInt(date[4]), parseInt(date[5])];
+
+  let [isValid, massage] = validateDate(dateParsed);
+  if (!isValid) {
+    return [false, massage];
+  } else {
+    return [true, gaussAlgorithm(dateParsed)];
+
+  }
+
 }
 
-function validateDate(date: number[]): boolean {
+function validateDate(date: number[]): [boolean, string] {
   let day = date[0];
   let month = date[1];
   let year = date[2];
 
+
+
   // check if year is valid
-  if (year < 0 || year > 9999) {
-    return false;
+  if (year < 0 || year > 99999) {
+    return [false, 'Invalid year (Max 5 digit)'];
   }
 
   // check if month if valid
   if (month < 1 || month > 12) {
-    return false;
+    return [false, 'Invalid month'];
   }
 
   // check if day is valid
@@ -38,10 +45,15 @@ function validateDate(date: number[]): boolean {
   }
 
   if (day < 1 || day > monthLength[month - 1]) {
-    return false;
+    return [false, 'Invalid day'];
   }
 
-  return true;
+  if (year < 1582 || (year === 1582 && month < 10) || (year === 1582 && month === 10 && day < 15)) {
+    return [false, 'Only consider Gregorian calendar after 15 October 1582'];
+  }
+
+  return [true, ''];
+
 }
 
 function gaussAlgorithm(date: number[]): string {
