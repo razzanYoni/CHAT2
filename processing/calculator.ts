20bca334c9a897ef5ad69625abe7f8a8fcdc8b83
Fakhrimm <16521155@mahasiswa.itb.ac.id>
2023-05-05 20:13:41 +0700

    fix: add docs and unhandled case in calculator.ts

--- processing/calculator.ts
+++ processing/calculator.ts
@@ -1,3 +1,10 @@
+/**
+ * @param a number pertama yang akan dioperasikan
+ * @param b number kedua yang akan dioperasikan
+ * @param op char operator yang akan digunakan
+ * @returns number hasil operasi a dan b dengan operator op
+ * 
+ */
 function op(a: number, b: number, op: string): number {
   switch (op) {
     case '+': return a + b
@@ -17,41 +24,79 @@ function op(a: number, b: number, op: string): number {
     default: throw new Error('Invalid operator')
   }
 }
+
+/**
+ * @param c char yang akan dicek apakah merupakandot
+ * @returns boolean true jika c merupakan dot, false jika tidak
+ */
 function isDot(c: string): boolean {
   return c === '.'
 }
 
+/**
+ * @param c char yang akan dicek apaha merupakan digit dengan dot 
+ * @returns boolean true jika c merupakan digit dengan dot, false jika tidak
+ */
 function isDigitWithDot(c: string): boolean {
   let RegExp = /^[0-9.]*\.[0-9.]*$/;
   return RegExp.test(c);
 }
 
+/**
+ * @param c char yang akan dicek apakah merupakan operator
+ * @returns boolean true jika c merupakan operator, false jika tidak
+ */
 function isOperator(c: string): boolean {
   return '+-*/^'.indexOf(c) !== -1
 }
-
+/**
+ * @param c char yang akan dicek apakah merupakan parenthesis
+ * @returns boolean true jika c merupakan parenthesis, false jika tidak
+ */
 function isParenthesis(c: string): boolean {
   return '()'.indexOf(c) !== -1
 }
-
+/**
+ * @param c char yang akan dicek apakah merupakan digit
+ * @returns boolean true jika c merupakan digit, false jika tidak
+ */
 function isDigit(c: string): boolean {
   let RegExp = /^[0-9]+$/
   return RegExp.test(c)
 }
 
+/**
+ * @param char yang akan dicek apakah merupakan number termaksud dot 
+ * @returns boolean true jika c merupakan number termasuk dot, false jika tidak
+ */
 function isNumber(c: string): boolean {
   let RegExp = /^[0-9.]+$/
   return RegExp.test(c)
 }
 
+/**
+ * @param c char yang akan dicek apakah merupakan left parenthesis
+ * @returns boolean true jika c merupakan left parenthesis, false jika tidak
+ */
 function isLeftParenthesis(c: string): boolean {
   return c === '('
 }
 
+/** 
+ * @param c char yang akan dicek apakah merupakan right parenthesis
+ * @returns booelean true jika c merupakan right parenthesis, false jika tidak
+ */
 function isRightParenthesis(c: string): boolean {
   return c === ')'
 }
 
+/**
+ * Sebuah fungsi yang mengembalikan input precedence dari sebuah operator yang valid
+ * precedence akan menentukan urutan operasi dari operator
+ * 
+ * @param c char yang akan dicari input precedence nya
+ * @returns input precedence dari c
+ */
 function inputPrecedence(c: string): number {
   switch (c) {
     case '+':
@@ -71,6 +116,13 @@ function inputPrecedence(c: string): number {
   }
 }
 
+/**
+ * Sebuah fungsi yang mengembalikan stack precedence dari sebuah operator yang valid
+ * precedence akan menentukan urutan operasi dari operator
+ * 
+ * @param c char yang akan dicari stack precedence nya
+ * @returns stastack precedence dari c
+ */
 function stackPrecedence(c: string): number {
   switch (c) {
     case '+':
@@ -87,7 +139,14 @@ function stackPrecedence(c: string): number {
       throw new Error('Invalid operator')
   }
 }
-
+/**
+ * Sebuah fungsi yang memproses masukan dengan notasi infix dalam bentuk string menjadi
+ * array of string dengan masing-masing elemen merupakan number, operator, atau parenthesis
+ * 
+ * fungsi juga melakukan pengecekan apakah masukan valid atau tidak
+ * @param exp string math expression dalam bentuk infix
+ * @returns array of string yang merupakan hasil dari pemrosesan exp
+ */
 export function processInfix(exp: string): string[] {
   let dotCount = 0
   let numberCount = 0
@@ -100,38 +159,56 @@ export function processInfix(exp: string): string[] {
   for (let i = 0; i < exp.length; i++) {
     let c = exp[i]
     if (isDigit(c)) {
+      /* 
+      pemrosesan jika c merupakan digit
+      apabila karakter sebelumnya merupakan digit atau dot, maka c akan digabungkan dengan digit sebelumnya
+      */
+
       if (result.length !== 0 && isDigit(result[result.length - 1])) {
         result[result.length - 1] += c
       } else if (result.length !== 0 && isDot(result[result.length - 1])) {
+        // Kasus karakter sebelumnya hanya dot, sehingga numberCount masih diincrement
+        dotCount = 0;
         result[result.length - 1] += c
         numberCount++
       } else if (result.length !== 0 && isDigitWithDot(result[result.length - 1])) {
         dotCount = 0;
         result[result.length - 1] += c
+      } else if (result.length !== 0 && isRightParenthesis(result[result.length - 1])) {
+        throw new Error('Cannot put number immediately after right parenthesis')
       } else {
+        // Kasus karakter sebelumnya merupakan operator atau left parenthesis
         result.push(c)
         numberCount++
       }
     } else if (isDot(c)) {
       if (result.length !== 0 && isDigitWithDot(result[result.length - 1])) {
+        // Kasus sudah ada dot sebelumnya
         throw new Error('Invalid dot position')
       }
-
       if (result.length !== 0 && isDigit(result[result.length - 1])) {
         result[result.length - 1] += c
+      } else if (result.length !== 0 && isRightParenthesis(result[result.length - 1])) {
+        throw new Error('Cannot put dot after right parenthesis')
       } else {
         result.push(c)
         dotCount++
       }
     } else if (isOperator(c)) {
       if (dotCount > 0) {
-        throw new Error('Invalid dot position')
+        throw new Error('Cannot have only dot as operand')
       }
       result.push(c)
       operatorCount++
     } else if (isLeftParenthesis(c)) {
+      if (result.length !== 0 && isRightParenthesis(result[result.length - 1])) {
+        throw new Error('Invalid parenthesis position')
+      }
+      if (result.length !== 0 && isNumber(result[result.length - 1])) {
+        throw new Error('Cannot have number or dot before left parenthesis')
+      }
       if (dotCount > 0) {
-        throw new Error('Invalid dot position')
+        throw new Error('Cannot have only dot as operand')
       }
       mapNumberCount[leftParanthesisCount] = numberCount
       mapOpCount[leftParanthesisCount] = operatorCount
@@ -139,7 +216,7 @@ export function processInfix(exp: string): string[] {
       result.push(c)
     } else if (isRightParenthesis(c)) {
       if (dotCount > 0) {
-        throw new Error('Invalid dot position')
+        throw new Error('Cannot have only dot as operand')
       }
       leftParanthesisCount--
       let opInsideParenthesis = operatorCount - mapOpCount[leftParanthesisCount]
@@ -159,7 +236,12 @@ export function processInfix(exp: string): string[] {
   }
   return result
 }
-
+/**
+ * Melakukan konversi dari infix ke postfix
+ * 
+ * @param exp array of string yang merupakan hasil dari pemrosesan exp
+ * @returns array of string yang merupakan hasil dari konversi exp ke postfix
+ */
 export function infixToPostfix(exp: string[]): string[] {
   let postfix: string[] = []
   let stack: string[] = ['(']
@@ -199,6 +281,12 @@ export function infixToPostfix(exp: string[]): string[] {
   return postfix
 }
 
+/**
+ * Evaluasi math expression yang sudah dalam bentuk postfix
+ * 
+ * @param exp array of string yang sudah dalam bentuk postfix
+ * @returns integer hasil evaluasi dari postfix
+ */
 export function evaluatePostfix(exp: string[]): number {
   let stack: number[] = []
   for (let i = 0; i < exp.length; i++) {
